fc89a42bd7ccb18bf0cb29f7fdcee39889877f9f
Malysheva Ksenia <maly6eva.ksenia@gmail.com>
2025-05-28 13:19:07 +0300

    useReducer с загрузкой данных

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
+import {useReducer, useEffect} from "react";
+
+const initialState = {
+    candidates: [],
+    newCandidate: '',
+    status: 'loading'
+}
+
+function reducer(state, action) {
+switch (action.type) {
+    case 'dataReceived':
+        return {
+            ...state,
+            candidates: action.payload,
+            status: 'ready',
+        }
+}
+}
+
 function VoteTracker() {
-  // If status is "ready", render main content
+  const [state, dispath] = useReducer(reducer, initialState)
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch('http://localhost:9000/candidates')
+            if(!res.ok){
+                throw new Error('Failed to fetch data')
+            }
+            const data = await res.json()
+            dispath({type: 'dataReceived', payload: data})
+        }
+        fetchData()
+    },[])
+
   return (
     <>
       <h1>Vote Tracker</h1>
       <ul>
-        <li>
-          Dima: 10 votes
-          <button>+</button>
-          <button>-</button>
-        </li>
+          {state.candidates.map((candidate) => (
+              <li key={candidate.id}>{candidate.name}: {candidate.votes} votes
+                  <button>+</button>
+                  <button>-</button>
+              </li>
+
+          ))}
       </ul>
       <button>Reset Votes</button>
 
